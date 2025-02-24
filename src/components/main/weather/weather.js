99a6d710fbd5e8372516cbe23c5c6074f794e2d7
Dinar2202 <Dinar2202@gmail.com>
2025-02-24 16:22:49 +0300

    feat: show date and generate card while no weather.

--- src/components/main/weather/weather.js
+++ src/components/main/weather/weather.js
@@ -2,7 +2,7 @@
 Задача какая, какие функции должны быть в погоде?
 1. Запрос данных с другого сервера и показ на странице по указанному городу. 
   Асинхронный запрос
-  
+
 2. Функциональный слайдер, возможнолсть переключаться между города. {
   2.1 Слайдер, основная по середине, остальные на половину скрыты и размыты. 
   2.2 При нажатие на кнопку, переключать на другой слайд, то есть на другой контейнер с погодой. {
@@ -13,4 +13,162 @@
   2.4     
 }
 
+++ 3. Время 
+
 */
+
+const TIME_BLOCK = document.querySelector(".weather-logo-block__time");
+const DATE_BLOCK = document.querySelector(".slider-card__data-text");
+
+const API_KEY = "a53c098ae82543b4a48124519252402";
+
+let nowDate = new Date();
+
+/* Вывод времени на странице*/
+
+function renderTime() {
+  let now = new Date();
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+
+  if (minutes <= 9) {
+    TIME_BLOCK.innerHTML = `${hours}:0${minutes}`;
+  } else {
+    TIME_BLOCK.innerHTML = `${hours}:${minutes}`;
+  }
+}
+
+renderTime();
+
+/* Отрисовка карточки с погодой на странице */
+
+function renderCardWeather() {
+  const sliderContainer = document.querySelector(".slider-container");
+
+  const sliderCard = document.createElement("div");
+  sliderCard.classList.add("slider-card");
+
+  const sliderLocationBLock = document.createElement("div");
+  sliderLocationBLock.classList.add("slider-card__location-block");
+
+  const sliderLocationText = document.createElement("p");
+  sliderLocationText.classList.add("slider-card__location-text");
+  sliderLocationText.textContent = "New York";
+
+  const sliderLocationImg = document.createElement("img");
+  sliderLocationImg.classList.add("slider-card__location-img");
+  sliderLocationImg.src = "public/svg/weather/location_.svg";
+  sliderLocationImg.alt = "location";
+
+  const sliderTempBlock = document.createElement("div");
+  sliderTempBlock.classList.add("slider-card__temp-block");
+
+  const sliderTempImg = document.createElement("img");
+  sliderTempImg.classList.add("slider-card__temp-img");
+  sliderTempImg.src = "public/svg/weather/temperature_.svg";
+  sliderTempImg.alt = "temperature";
+
+  const sliderTempMeaning = document.createElement("p");
+  sliderTempMeaning.classList.add("slider-card__temp-meaning");
+  sliderTempMeaning.textContent = "27°C";
+
+  const sliderDataBlock = document.createElement("div");
+  sliderDataBlock.classList.add("slider-card__data-block");
+
+  const sliderDataText = document.createElement("p");
+  sliderDataText.classList.add("slider-card__data-text");
+  let month = getMonth(nowDate);
+  let weekDay = getWeekDay(nowDate);
+  let dayDate = nowDate.getDate();
+  sliderDataText.textContent = `${month} ${dayDate}, ${weekDay}`;
+
+  const sliderAdditionalBLock = document.createElement("div");
+  sliderAdditionalBLock.classList.add("slider-card__additional-block");
+
+  const sliderAdditionalList = document.createElement("ul");
+  sliderAdditionalList.classList.add("slider-card__additional__list");
+
+  const sliderAdditionalItem = document.createElement("li");
+  sliderAdditionalItem.classList.add("slider-card__additional__item");
+  sliderAdditionalItem.textContent = "Humidity";
+  const sliderAdditionalItemSpan = document.createElement("span");
+  sliderAdditionalItemSpan.textContent = "99%";
+
+  sliderContainer.appendChild(sliderCard);
+  sliderCard.appendChild(sliderLocationBLock);
+  sliderLocationBLock.appendChild(sliderLocationText);
+  sliderLocationBLock.appendChild(sliderLocationImg);
+  sliderCard.appendChild(sliderTempBlock);
+  sliderTempBlock.appendChild(sliderTempImg);
+  sliderTempBlock.appendChild(sliderTempMeaning);
+  sliderCard.appendChild(sliderDataBlock);
+  sliderDataBlock.appendChild(sliderDataText);
+  sliderCard.appendChild(sliderAdditionalBLock);
+  sliderAdditionalBLock.appendChild(sliderAdditionalList);
+
+  sliderAdditionalList.appendChild(sliderAdditionalItem);
+  sliderAdditionalItem.appendChild(sliderAdditionalItemSpan);
+}
+
+renderCardWeather();
+
+function getMonth(nowDate) {
+  let month = nowDate.getMonth();
+
+  switch (month) {
+    case 0:
+      return "Jun";
+    case 1:
+      return "Feb";
+    case 2:
+      return "Mar";
+    case 3:
+      return "Apr";
+    case 4:
+      return "May";
+    case 5:
+      return "Jun";
+    case 6:
+      return "Jul";
+    case 7:
+      return "Aug";
+    case 8:
+      return "Sep";
+    case 9:
+      return "Oct";
+    case 10:
+      return "Nov";
+    case 11:
+      return "Dec";
+  }
+}
+
+function getWeekDay(nowDate) {
+  let weekDay = nowDate.getDay();
+
+  switch (weekDay) {
+    case 0:
+      return "Sun";
+    case 1:
+      return "Mon";
+    case 2:
+      return "Tues";
+    case 3:
+      return "Wed";
+    case 4:
+      return "Thurs";
+    case 5:
+      return "Fri";
+    case 6:
+      return "Sat";
+  }
+}
+
+fetch(`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=Moscow`)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
+
+  /* Задача!
+    Внедрить функцию, которая будет обрабатывать запрос и рендирить карточку полностью с погодой.
+  
+  */
